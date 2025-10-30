f6a1d0f928d02cf0d1ebb2f9d90935daee18b413
Rinchen lodoe <rinchenstrnd@gmail.com>
2025-10-30 21:50:07 +0000

    save

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,17 +1,15 @@
 select
-    
-    t1.* except (quantity),
-    t2.purchase_price,
 
     t1.quantity,
+
+    t2.purchse_price,
+
     t1.revenue,
 
     cast(t1.quantity as int64) * t2.purchase_price as purchase_cost,
 
-    
     t1.revenue - (cast(t1.quantity as int64) * t2.purchase_price) as margin,
 
-from
-    {{ ref("stg_raw__sales.sql") }} as t1  
-    
-inner join {{ ref("stg_raw__product.sql") }} as t2 on t1.products_id = t2.products_id
+from {{ ref("stg_raw__sales") }} as t1
+
+inner join {{ ref("stg_raw__product") }} as t2 on t1.products_id = t2.products_id
