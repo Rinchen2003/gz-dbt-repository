e8e5d9c380f8dac4a9f7e9050756d7669b7284c8
Rinchen lodoe <rinchenstrnd@gmail.com>
2025-10-30 12:20:57 +0000

    saving changes

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,18 @@
+with
+
+    source as (select * from {{ source("raw", "ship") }}),
+
+    renamed as (
+
+        select
+            orders_id,
+            shipping_fee,
+            logcost as logcost,
+            cast(ship_cost as int64) as ship_cost
+
+        from source
+
+    )
+
+select *
+from renamed
