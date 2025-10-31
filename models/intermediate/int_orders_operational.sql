6c3b766ee4f6636970cf4ca58d68f5430ccb0ad1
Rinchen lodoe <rinchenstrnd@gmail.com>
2025-10-31 14:44:41 +0000

    changes

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,6 +1,5 @@
-
 SELECT
-   
+    
     o.orders_id,
     o.date_date,
     o.quantity,
@@ -8,38 +7,26 @@ SELECT
     o.purchase_cost,
     o.margin,
     
+    
     s.shipping_fee,
     s.log_cost,
     s.ship_cost,
     
-  
+    
+    
     ROUND(
         o.margin 
         + SAFE_CAST(s.shipping_fee AS FLOAT64) 
-        - (SAFE_CAST(s.log_cost AS FLOAT64) + SAFE_CAST(s.ship_cost AS FLOAT64)), 
-    2) AS operational_margin,
+        - SAFE_CAST(s.log_cost AS FLOAT64) 
+        - SAFE_CAST(s.ship_cost AS FLOAT64), 
+    2) AS operational_margin
     
 FROM
-    {{ref("int_orders_margin")}} AS o 
-LEFT JOIN
-    {{ref("stg_raw__ship")}} AS s      
-    USING(orders_id) 
     
+    {{ref('int_orders_margin')}} AS o 
+LEFT JOIN
+    
+    {{ref('stg_raw__ship')}} AS s      
+    ON o.orders_id = s.orders_id 
 ORDER BY 
-    orders_id DESC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    o.orders_id DESC
